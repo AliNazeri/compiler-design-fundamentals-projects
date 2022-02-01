9f7545e2f78d5294e66e2b1a9b5370eb5fbc60aa
AliNazeri <erfan.nazeri@gmail.com>
2022-02-01 13:15:42 +0330

    test file added

---
+++ preprocess/main.c
@@ -0,0 +1,15 @@
+// main
+int main()
+{
+    //int
+    int tc,n,i;
+    scanf("%d",&tc);
+    /* 
+    gg
+    gj*/
+    while(tc--){
+        scanf("%d",&n);
+        for(i=1;i<=n;i++) printf("%d ",i);
+        printf("\n");
+    }
+}
